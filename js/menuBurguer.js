52114bf8496408e2c0fe7b7c2a178dcb04c81077
leonardobarbosas <leonardobarbosasantos@gmail.com>
2024-06-07 13:53:56 -0300

    adicionando menu burguer

---
+++ js/menuBurguer.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const menu = document.querySelector(".menu-burguer-container");
+
+function menuAction() {
+  menu.classList.toggle("show");
+}
+
+menu.addEventListener("click", menuAction);
